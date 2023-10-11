c34c64654cb150344666738030756ef2b92f02cd
Reinaldo Ramos <masterrey@gmail.com>
2023-10-11 10:48:40 -0300

    tempo iluminacao e cenas

--- Assets/Code/PlayerCharacter.cs
+++ Assets/Code/PlayerCharacter.cs
@@ -1,15 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerCharacter : TopCharacter
 {
-    
+    [SerializeField]
+    string leveltoload;
 
     protected override void MyInput()
     {
-        
+        ChangeTimeLevel();
         base.move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    private void ChangeTimeLevel()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            SceneManager.LoadScene(leveltoload);
+        }
+        if (Input.GetButtonDown("Fire3"))
+        {
+            PlayerPrefs.DeleteAll();
+        }
     }
 
 
